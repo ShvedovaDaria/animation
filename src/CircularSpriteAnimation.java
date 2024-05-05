d83a335b08b53a94b54eb563ca9c5c9dc13bb1b5
ShvedovaDaria <dashashvedova07@gmail.com>
2024-05-05 16:22:54 +0300

    initial commit

--- src/CircularSpriteAnimation.java
+++ src/CircularSpriteAnimation.java
@@ -1,37 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CircularSpriteAnimation extends JFrame {
 
-    private SpritePanel spritePanel;
-    private Timer timer;
-    private double angle;
+    private SpriteModel model;
+    private SpriteView view;
+    private SpriteController controller;
 
     public CircularSpriteAnimation() {
         setTitle("Circular Sprite Animation");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        spritePanel = new SpritePanel();
-        add(spritePanel);
+        model = new SpriteModel();
+        view = new SpriteView();
+        controller = new SpriteController(model, view);
 
-        angle = 0;
+        add(view);
 
-        timer = new Timer(50, e -> {
-            moveSprite();
-            spritePanel.repaint();
-        });
+        Timer timer = new Timer(50, e -> controller.moveSprite());
         timer.start();
-    }
-
-    private void moveSprite() {
-        // Збільшуємо кут для кругового руху
-        angle += Math.toRadians(5);
-        // Обчислюємо нові координати спрайта
-        int x = (int) (getWidth() / 2 + Math.cos(angle) * 200); // Радіус 200 пікселів
-        int y = (int) (getHeight() / 2 + Math.sin(angle) * 200); // Радіус 200 пікселів
-        // Встановлюємо нові координати для спрайта
-        spritePanel.setSpritePosition(x, y);
     }
 
     public static void main(String[] args) {
@@ -40,30 +30,67 @@ public class CircularSpriteAnimation extends JFrame {
             animation.setVisible(true);
         });
     }
+}
 
-    private class SpritePanel extends JPanel {
+class SpriteModel {
+    private double angle;
 
-        private ImageIcon spriteImage;
-        private int x, y;
+    public SpriteModel() {
+        angle = 0;
+    }
 
-        public SpritePanel() {
-            // Завантажуємо зображення спрайта з ресурсів
-            spriteImage = new ImageIcon(getClass().getResource("/sprite.png"));
-            setSize(spriteImage.getIconWidth(), spriteImage.getIconHeight());
-            x = getWidth() / 2;
-            y = getHeight() / 2;
-        }
+    public void increaseAngle() {
+        angle += Math.toRadians(5);
+    }
 
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            g.drawImage(spriteImage.getImage(), x, y, this);
-        }
+    public int calculateX(int width) {
+        return (int) (width / 2 + Math.cos(angle) * 200);
+    }
 
-        public void setSpritePosition(int x, int y) {
-            this.x = x - spriteImage.getIconWidth() / 2;
-            this.y = y - spriteImage.getIconHeight() / 2;
-            repaint();
-        }
+    public int calculateY(int height) {
+        return (int) (height / 2 + Math.sin(angle) * 200);
+    }
+}
+
+class SpriteView extends JPanel {
+
+    private ImageIcon spriteImage;
+    private int x, y;
+
+    public SpriteView() {
+        spriteImage = new ImageIcon(getClass().getResource("/sprite.png"));
+        setSize(spriteImage.getIconWidth(), spriteImage.getIconHeight());
+        x = getWidth() / 2;
+        y = getHeight() / 2;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(spriteImage.getImage(), x, y, this);
+    }
+
+    public void setSpritePosition(int x, int y) {
+        this.x = x - spriteImage.getIconWidth() / 2;
+        this.y = y - spriteImage.getIconHeight() / 2;
+        repaint();
+    }
+}
+
+class SpriteController {
+
+    private SpriteModel model;
+    private SpriteView view;
+
+    public SpriteController(SpriteModel model, SpriteView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void moveSprite() {
+        model.increaseAngle();
+        int x = model.calculateX(view.getWidth());
+        int y = model.calculateY(view.getHeight());
+        view.setSpritePosition(x, y);
     }
 }
